9c0dff08d97421265a93bb21f74936c57b627464
Robin Unger <robin.e.unger@gmail.com>
2018-11-09 19:38:49 -0600

    added 'etch' mouseover functionality

--- etch.js
+++ etch.js
@@ -1,10 +1,14 @@
 const gridLength = 100;
 const gridArea = gridLength * gridLength;
-const container = document.querySelector('.container');
-container.setAttribute('style', `grid-template-columns: repeat(${gridLength}, auto);`);
+const canvas = document.querySelector('.canvas');
+canvas.setAttribute('style', `grid-template-columns: repeat(${gridLength}, auto);`);
 
 for (i = 0; i < gridArea; i++) {
     const cell = document.createElement('div');
+    cell.addEventListener('mouseenter', function(e) {
+        this.setAttribute('style', 'background: gray');
+    });
     cell.classList.add('cell');
-    container.appendChild(cell);
+    canvas.appendChild(cell);
 }
+
