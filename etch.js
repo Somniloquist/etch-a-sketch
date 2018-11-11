9e22ce1af7ecf6c317048f7c42295255dfcfdb76
Robin Unger <robin.e.unger@gmail.com>
2018-11-11 13:26:26 -0600

    -fixed bug where canvas cells were not being reset upon resizing. -added delete cells functionality

--- etch.js
+++ etch.js
@@ -1,23 +1,65 @@
-function createCanvas(canvasLength = 10) {
+
+// need to find a way to reset div cells
+
+function drawCanvas(canvasLength = 10) {
     const canvas = document.querySelector('.canvas');
     const canvasArea = canvasLength * canvasLength;
     canvas.setAttribute('style', `grid-template-columns: repeat(${canvasLength}, auto);`);
 
+    // delete existing cells on the canvas
+    deleteCells();
+
+    // fill the canvas with cells
     for (i = 0; i < canvasArea; i++) {
         const cell = document.createElement('div');
         cell.addEventListener('mouseenter', function(e) {
-            this.setAttribute('style', 'background: gray');
+            this.setAttribute('style', `background: ${cursorColor}`);
         });
         cell.classList.add('cell');
         canvas.appendChild(cell);
     }
 }
 
-function reset() {
-    cells = document.querySelectorAll('.cell[style]');
+function deleteCells() {
+    canvas = document.querySelector('.canvas');
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+}
+
+function resizeCanvas() {
+    clear();
+    drawCanvas(getCanvasSize());
+}
+
+function getCanvasSize() {
+    return prompt('Enter the amount of squares for each side of the canvas.');
+}
+
+
+function setCursorColor(color = 'gray') {
+    cursorColor = color;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomColor() {
+    return `rgba(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)}, ${100})`
+}
+
+function clear() {
+    const cells = document.querySelectorAll('.cell[style]');
     for(i = 0; i < cells.length; i++) {
         cells[i].removeAttribute('style');
     }
 };
 
-createCanvas();
+const clearBtn = document.getElementById('clearCanvas');
+clearBtn.addEventListener('click', clear);
+const resizeBtn = document.getElementById('resizeCanvas');
+resizeBtn.addEventListener('click', resizeCanvas);
+let cursorColor = 'gray';
+
+drawCanvas();
